148423d2effd1e0a072781cf21aa226aaad1c572
ma <ma@infra.guru>
2018-10-26 18:50:03 +0300

    Create scripts for touches detection

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private readonly TouchesProxy touches = new TouchesProxy();
+
+    private readonly Action<Vector2> onTouchBegan = (Vector2 position) => {
+        Debug.Log("Touch began");
+    };
+
+    private readonly Action<Vector2> onTouchMoved = (Vector2 position) => {
+        Debug.Log("Touch moved");
+    };
+
+    private readonly Action onTouchFinished = () => {
+        Debug.Log("Touch finished");
+    };
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        touches.Update(onTouchBegan, onTouchMoved, onTouchFinished);
+    }
+
+}

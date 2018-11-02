739252f600ab8f0109d0703c7c9d3343f8bcd1af
ma <ma@infra.guru>
2018-11-02 10:39:35 +0300

    Add JetBrans Rider Plugin

---
+++ Assets/Scripts/SwipeDirection.cs
@@ -0,0 +1,1 @@
+﻿public enum SwipeType { LEFT, TOP, RIGHT, BOTTOM }
---
+++ Assets/Scripts/TouchesDelegate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class TouchesProxy {
+
+    private readonly Action<Vector2> _onTouchBegan;
+    private readonly Action<Vector2> _onTouchMoved;
+    private readonly Action _onTouchFinished;
+
+    public TouchesProxy(Action<Vector2> onTouchBegan,
+                        Action<Vector2> onTouchMoved,
+                        Action onTouchFinished) {
+        _onTouchBegan = onTouchBegan;
+        _onTouchMoved = onTouchMoved;
+        _onTouchFinished = onTouchFinished;
+    }
+
+    public void Update() {
+#if UNITY_IOS || UNITY_ANDROID
+        Touch touch;
+        if (Input.touchCount == 1) {
+            touch = Input.GetTouch(0);
+        }
+        else {
+            return;
+        }
+
+        switch (touch.phase) {
+            case TouchPhase.Began:
+                _onTouchBegan(touch.position);
+                break;
+
+            case TouchPhase.Moved:
+                _onTouchMoved(touch.position);
+                break;
+
+            default:
+                _onTouchFinished();
+                break;
+        }
+#elif UNITY_EDITOR || UNITY_STANDALONE
+        if (Input.GetMouseButtonDown(0)) {
+            _onTouchBegan(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0)) {
+            _onTouchMoved(Input.mousePosition);
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            _onTouchFinished();
+        }
+#endif
+    }
+
+}

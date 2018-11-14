6413ceb4e6ae5588bd899ee0a8095e46a9f4017c
ma <ma@infra.guru>
2018-11-14 23:28:23 +0300

    Create CameraInterpolator

---
+++ Assets/Scripts/CameraInterpolator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraInterpolator {
+
+	private const float Tolerance = 0.1f;
+	private const float CameraOffset = 2.0f;
+	private const float N = 20f;
+	private float _groundY;
+	private float _cameraY;
+	private float _step;
+
+	public float GetCameraY(float groundY) {
+		var groundDelta = Mathf.Abs(groundY - _groundY);
+		var desiredCameraY = groundY + CameraOffset;
+		
+		if (groundDelta > Tolerance) {
+			_groundY = groundY;
+			_step = (desiredCameraY - _cameraY) / N;
+		}
+
+		if (Mathf.Abs(_cameraY + _step - desiredCameraY) < Mathf.Abs(_cameraY - desiredCameraY)) {
+			_cameraY += _step;
+		}
+
+		return _cameraY;
+	}
+	
+}
